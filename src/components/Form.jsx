91a8f3764ac5a629d3ee8f0d691e28a57ec2d7ad
Joseph Lander <josephclander@gmail.com>
2022-12-25 13:31:01 +0000

    Add submit function

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,19 +1,37 @@
-import React from 'react';
+import { useRef } from 'react';
 
 const Form = () => {
+  const inputEmail = useRef(null);
+  const inputPassword = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+      email: inputEmail.current?.value,
+      password: inputPassword.current?.value,
+    };
+    e.target.reset();
+    console.log({ data });
+  };
+
   return (
-    <form className='form' action='#'>
+    <form className='form' onSubmit={handleSubmit}>
       <label name='email'>
         {' '}
         Email:
-        <input className='form__email' type='email' />
+        <input className='form__email' type='email' ref={inputEmail} />
       </label>
       <label name='password'>
         {' '}
         Password:
-        <input className='form__password' type='password' />
+        <input className='form__password' type='password' ref={inputPassword} />
       </label>
-      <input className='form__submit' type='submit' value='Submit' />
+      <input
+        className='form__submit'
+        type='submit'
+        value='Submit'
+        onSubmit={handleSubmit}
+      />
     </form>
   );
 };
